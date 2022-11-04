78d573ead2fb7953555fc663cec06323ae8afee1
_ Tanushree (tanushree7@deloitte.com) <tanushree7@your.hostname.com>
2022-11-04 10:14:55 +0530

    added

--- src/App.js
+++ src/App.js
@@ -1,30 +1,37 @@
-import React, { Component } from 'react'
-import Dashboard from './components/dashboard/Dashboard'
-import './App.css'
-import Header from './components/layout/Header'
-import 'bootstrap/dist/css/bootstrap.css'
-import { BrowserRouter as Router,Route,Routes } from 'react-router-dom'
-import AddElection from './components/elections/AddElection'
-import Login from './components/userManagement/Login'
-import Election from './components/elections/Election'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import AddElection from "./components/AddElection";
+import AssignRole from "./components/AssignRole";
+import Header from "./components/UserManagement/Header";
+import UserLogin from "./components/UserManagement/UserLogin";
+import AdminLogin from "./components/AdminPanel/AdminLogin";
+import ActivateElection from "./components/ActivateElection";
+import AdminHeader from "./components/AdminPanel/AdminHeader";
+import MainDashBoard from "./components/MainDashBoard";
+import { Component } from "react";
+import ShowElections from "./components/ShowElections";
 export default class App extends Component {
   render() {
     return (
       <Router>
-        <Header/>
-        
         <Routes>
-        {/* Public routes */}
-        <Route exact path="/" element={<Dashboard/>}/>
-       
-        {/* Private routes */}
-        <Route exact path="/dashboard" element={<Dashboard/>}/>
-        <Route exact path="/addElection" element={<AddElection/>}/>
-        <Route exact path="/showElections" element={<Election/>}/>
-        <Route exact path="/adminLogin" element={<Login/>}></Route>
+          {/* Public Routes */}
+          <Route exact path="/" element={<MainDashBoard />} />
+          {/* Private Routes */}
+          <Route exact path="/dashboard" element={<MainDashBoard />} />
+          <Route exact path="/assignrole" element={<AssignRole />} />
+          <Route exact path="/addElection" element={<AddElection />} />
+          <Route exact path="/showElections" element={<ShowElections />} />
+          <Route exact path="/adminLogin" element={<AdminLogin />}></Route>
         </Routes>
+        {/* <NavBar></NavBar> */}
+        {/* <AddElection /> */}
+        {/* <AssignRole /> */}
+        {/* <AdminHeader /> */}
+        {/* <ActivateElection /> */}
+        {/* <Header /> */}
+        {/* <MainDashBoard /> */}
+        {/* <AdminLogin /> */}
       </Router>
-    )
+    );
   }
 }
-
